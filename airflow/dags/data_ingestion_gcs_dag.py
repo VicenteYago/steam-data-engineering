d5ad6866e1f01998ebd61494d436a17263e03568
Yago <josevicente.yago@um.es>
2022-09-11 06:31:42 +0200

    eod commit airflow

--- airflow/dags/data_ingestion_gcs_dag.py
+++ airflow/dags/data_ingestion_gcs_dag.py
@@ -15,19 +15,14 @@ import pyarrow.parquet as pq
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
+#dataset_file = "yellow_tripdata_2021-01.csv"
+#dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+#parquet_file = dataset_file.replace('.csv', '.parquet')
+#BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
 
 
-def format_to_parquet(src_file):
-    if not src_file.endswith('.csv'):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
 
 
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
@@ -71,15 +66,13 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        bash_command=f"kaggle datasets download -d souyama/steam-dataset"
     )
 
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-        },
+
+    unzip_dataset_task = BashOperator(
+        task_id="unzip_dataset_task",
+        bash_command=f"unzip steam-dataset.zip"
     )
 
     # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
@@ -88,24 +81,12 @@ with DAG(
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            #"object_name": f"raw/{parquet_file}",
+            "object_name": f"raw/steamspy_basic.json",
+            #"local_file": f"{path_to_local_home}/{parquet_file}",
+            "local_file": f"{path_to_local_home}steam_dataset/steamspy/basic/steam_spy_scrap.json",
         },
     )
 
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-            },
-        },
-    )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task >> unzip_dataset_task >> local_to_gcs_task
