a6ed1c5e5a3f717900e661d6fb2d043102be3069
jvyagomartinez@gmail.com <jvyagomartinez@gmail.com>
2022-09-15 12:50:39 +0000

    steam_spy_scrap model OK

--- dbt/models/staging/steam_spy_scrap.sql
+++ dbt/models/staging/steam_spy_scrap.sql
@@ -10,15 +10,14 @@ select
     SAFE_CAST(positive as integer) as positive,
     SAFE_CAST(negative as integer) as negative,
     SAFE_CAST(userscore as integer) as userscore,
-    owners,
+    SAFE_CAST( REPLACE( REGEXP_EXTRACT(owners, r'(.*)\.\.'), ",", "") AS INTEGER) AS owners_low,
+    SAFE_CAST( REPLACE( REGEXP_EXTRACT(owners, r'\.\.(.*)'), ",", "") AS INTEGER) AS owners_high,
     SAFE_CAST(average_forever as integer) as average_forever,
     SAFE_CAST(average_2weeks as integer) as average_2weeks,
     SAFE_CAST(median_forever as integer) as median_forever,
     SAFE_CAST(median_2weeks as integer) as median_2weeks,
-    SAFE_CAST(price as decimal) as price,
-    SAFE_CAST(initialprice as decimal) as initialprice,
-    SAFE_CAST(discount as decimal) as discount,
+    SAFE_CAST(price as decimal)/100 as price,
+    SAFE_CAST(initialprice as decimal)/100 as initialprice,
+    SAFE_CAST( REPLACE(discount, '.0', '' ) as integer) as discount_percentage,
     SAFE_CAST(ccu as integer) as ccu
 from  {{source('staging', 'steam_spy_scrap')}}
-limit 100
-
