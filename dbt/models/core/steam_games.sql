ebbade091ae75a5999d8cfb3c6671c23994420a4
jvyagomartinez@gmail.com <jvyagomartinez@gmail.com>
2022-11-01 02:18:39 +0000

    duplicates remove w/ cte & row_number

--- dbt/models/core/steam_games.sql
+++ dbt/models/core/steam_games.sql
@@ -1,5 +1,6 @@
 {{ config(
-    materialized='table',
+    materialized='incremental',
+    unique_key='appid',
     partition_by={
       "field": "release_year",
       "data_type": "int64",
@@ -11,38 +12,72 @@
     }
 )}}
 
+with t as 
+(
+  SELECT 
+        steam_store_data.appid,
+        steam_store_data.type,
+        steam_store_data.name,
+        steam_store_data.developers,
+        steam_store_data.publishers,
+        steam_store_data.dlcs,
+        steam_store_data.metacritic,
+        steam_store_data.required_age,
+        steam_store_data.is_free,
+        steam_store_data.release_year,
+        steam_store_data.coming_soon, 
+        steam_store_data.controller_support,
+        steam_store_data.demos_appid,
+        steam_store_data.drm_notice,
+        steam_store_data.recommendations,
+        steam_store_data.categories as categories,
+        steam_store_data.genres as genres,
+        STRUCT<low integer, high integer>(steam_spy_scrap.owners_low, steam_spy_scrap.owners_high) as owners,
+        STRUCT<low decimal, high decimal> (steam_spy_scrap.owners_low * steam_spy_scrap.price,
+                                        steam_spy_scrap.owners_high*steam_spy_scrap.price) as revenue,
+        STRUCT<windows boolean,
+                mac     boolean,
+                linux   boolean>(steam_store_data.platform_windows, steam_store_data.platform_mac, steam_store_data.platform_linux) as platforms,
+        steam_spy_scrap.negative,
+        steam_spy_scrap.positive,
+        steam_spy_scrap.price,
+        steam_spy_scrap.initialprice,
+        steam_spy_scrap.discount_percentage,
+        
+        steam_spy_scrap.ccu, 
+        ROW_NUMBER() over(partition by steam_store_data.appid) as rnk
 
-SELECT steam_store_data.appid,
-       steam_store_data.type,
-       steam_store_data.name,
-       steam_store_data.developers,
-       steam_store_data.publishers,
-       steam_store_data.dlcs,
-       steam_store_data.metacritic,
-       steam_store_data.required_age,
-       steam_store_data.is_free,
-       steam_store_data.release_year,
-       steam_store_data.coming_soon, 
-       steam_store_data.controller_support,
-       steam_store_data.demos_appid,
-       steam_store_data.drm_notice,
-       steam_store_data.recommendations,
-       steam_store_data.categories as categories,
-       steam_store_data.genres as genres,
-       STRUCT<low integer, high integer>(steam_spy_scrap.owners_low, steam_spy_scrap.owners_high) as owners,
-       STRUCT<low decimal, high decimal> (steam_spy_scrap.owners_low * steam_spy_scrap.price,
-                                      steam_spy_scrap.owners_high*steam_spy_scrap.price) as revenue,
-       STRUCT<windows boolean,
-              mac     boolean,
-              linux   boolean>(steam_store_data.platform_windows, steam_store_data.platform_mac, steam_store_data.platform_linux) as platforms,
-       steam_spy_scrap.negative,
-       steam_spy_scrap.positive,
-       steam_spy_scrap.price,
-       steam_spy_scrap.initialprice,
-       steam_spy_scrap.discount_percentage,
-       
-       steam_spy_scrap.ccu
+  FROM {{ref('steam_store_data')}} as steam_store_data LEFT JOIN  
+            {{ref('steam_spy_scrap')}}  as steam_spy_scrap ON steam_spy_scrap.appid = steam_store_data.appid
+  WHERE release_year <=2025
+)
 
-FROM {{ref('steam_store_data')}} as steam_store_data LEFT JOIN  
-          {{ref('steam_spy_scrap')}}  as steam_spy_scrap ON steam_spy_scrap.appid = steam_store_data.appid
-WHERE release_year <=2025
+
+SELECT 
+  appid,
+  type,
+  name,
+  developers,
+  publishers,
+  dlcs,
+  metacritic,
+  required_age,
+  is_free,
+  release_year,
+  coming_soon, 
+  controller_support,
+  demos_appid,
+  drm_notice,
+  recommendations,
+  categories as categories,
+  genres as genres,
+  owners,
+  revenue,
+  platforms,
+  negative,
+  positive,
+  price,
+  initialprice,
+  discount_percentage,
+  ccu
+FROM t WHERE rnk = 1
--- dbt/models/core/steam_reviews.sql
+++ dbt/models/core/steam_reviews.sql
@@ -1,28 +1,53 @@
 {{ config(materialized='table') }}
 
+with t as (
+    SELECT 
+        SAFE_CAST(recommendationid as INTEGER) as reviewid,			
+        comment_count,			
+        language,			
+        received_for_free,			
+        review,			
+        steam_purchase,		
+        timestamp_created,		
+        timestamp_updated,			
+        voted_up,			
+        votes_funny,			
+        votes_up,			
+        SAFE_CAST(weighted_vote_score as DECIMAL) as weighted_vote_score,			
+        written_during_early_access,		
+        SAFE_CAST(gameid as INTEGER) as gameid,			
+        author_last_played,		
+        author_num_games_owned,			
+        author_num_reviews,			
+        author_playtime_at_review,			
+        author_playtime_forever,			
+        author_playtime_last_two_weeks,			
+        CAST(author_steamid as INTEGER) as author_steamid, 
+        ROW_NUMBER() over(partition by recommendationid) as rnk
+    FROM  {{source('raw', 'steam_reviews')}}
+)
+ 
+select 
+    reviewid,			
+    comment_count,			
+    language,			
+    received_for_free,			
+    review,			
+    steam_purchase,		
+    timestamp_created,		
+    timestamp_updated,			
+    voted_up,			
+    votes_funny,			
+    votes_up,			
+    weighted_vote_score,			
+    written_during_early_access,		
+    gameid,			
+    author_last_played,		
+    author_num_games_owned,			
+    author_num_reviews,			
+    author_playtime_at_review,			
+    author_playtime_forever,			
+    author_playtime_last_two_weeks,			
+    author_steamid
+from t where rnk = 1
 
-SELECT 
-
-comment_count,			
-language,			
-received_for_free,			
-SAFE_CAST(recommendationid as INTEGER) as recommendationid ,			
-review,			
-steam_purchase,		
-timestamp_created,		
-timestamp_updated,			
-voted_up,			
-votes_funny,			
-votes_up,			
-SAFE_CAST(weighted_vote_score as DECIMAL) as weighted_vote_score,			
-written_during_early_access,		
-SAFE_CAST(gameid as INTEGER) as gameid,			
-author_last_played,		
-author_num_games_owned,			
-author_num_reviews,			
-author_playtime_at_review,			
-author_playtime_forever,			
-author_playtime_last_two_weeks,			
-CAST(author_steamid as INTEGER) as author_steamid 
-
-from  {{source('raw', 'steam_reviews')}}
